861ffd885cf6362fa3201c1874a9076d6b55654b
mancinibrzl <mariomancinineto2017@gmail.com>
2024-06-27 14:36:59 -0300

    adding run key (left shift)

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -9,8 +9,11 @@ public class Player : MonoBehaviour
     public Vector2 friction = new Vector2(.1f, 0);
 
     public float speed;
+    public float speedRun;
 
     public float forceJump = 2;
+
+    private float _currentSpeed;
 
     private void Update()
     {
@@ -20,15 +23,20 @@ public class Player : MonoBehaviour
 
     private void HandleMoviment()
     {
+        if (Input.GetKey(KeyCode.LeftShift))
+            _currentSpeed = speedRun;
+        else
+            _currentSpeed = speed;
+
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             //myRigidBody.MovePosition(myRigidBody.position - velocity * Time.deltaTime);
-            myRigidBody.velocity = new Vector2(-speed, myRigidBody.velocity.y);
+            myRigidBody.velocity = new Vector2(-_currentSpeed, myRigidBody.velocity.y);
         }
         else if (Input.GetKey(KeyCode.RightArrow))
         {
             //myRigidBody.MovePosition(myRigidBody.position + velocity * Time.deltaTime);
-            myRigidBody.velocity = new Vector2(speed, myRigidBody.velocity.y);
+            myRigidBody.velocity = new Vector2(_currentSpeed, myRigidBody.velocity.y);
         }
 
         if (myRigidBody.velocity.x > 0)
