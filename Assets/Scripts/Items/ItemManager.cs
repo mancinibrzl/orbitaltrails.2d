b5876df17b98d25dfd2675a11bc056f6f77d4f25
mancinibrzl <mariomancinineto2017@gmail.com>
2024-07-18 18:15:52 -0300

    update itemmanager script to start contabilize coins in the UI

--- Assets/Scripts/Items/ItemManager.cs
+++ Assets/Scripts/Items/ItemManager.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Mancini.Core.Singleton;
+using TMPro;
 
 public class ItemManager : Singleton<ItemManager>
 {
     public int coins;
+    [SerializeField] private TMP_Text _coinsText;
 
     private void Start()
     {
@@ -15,10 +17,17 @@ public class ItemManager : Singleton<ItemManager>
     private void Reset()
     {
         coins = 0;
+        UpdateCoinsText();
     }
 
     public void AddCoins(int amount = 1)
     {
         coins += amount;
+        UpdateCoinsText();
+    }
+
+    private void UpdateCoinsText()
+    {
+        _coinsText.text = "x " + coins.ToString();
     }
 }
